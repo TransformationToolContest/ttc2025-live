dcf1e3d9820a3943835c5972022eccaede90ddbd
Vadim Zaytsev <vadim@grammarware.net>
2025-06-15 23:35:17 +0200

    add a module with unit tests

---
+++ solutions/SLE/Tests/UnitTests.fs
@@ -0,0 +1,24 @@
+﻿namespace Tests
+
+open SLE.Core
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type UnitTests () =
+
+    [<TestMethod>]
+    member this.Trivial () =
+        Assert.IsTrue(true)
+        
+    [<TestMethod>]
+    member this.FeatureName () =
+        let feature = makeFeature "engine"
+        Assert.AreEqual<string>("engine", feature.Name)
+
+    [<TestMethod>]
+    member this.FreshInnersEmpty () =
+        let feature = makeFeature "foo"
+        Assert.AreEqual<int>(0, feature.Mandatory.Count)
+        Assert.AreEqual<int>(0, feature.Optional.Count)
+        Assert.AreEqual<int>(0, feature.Alternative.Count)
+        Assert.AreEqual<int>(0, feature.Or.Count)
