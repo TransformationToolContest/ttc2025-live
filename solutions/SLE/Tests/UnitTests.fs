79b68c2bf4a541003e2b8bd13fc67ef38e72fc52
Vadim Zaytsev <vadim@grammarware.net>
2025-06-16 17:39:14 +0200

    add two actual test cases (sadly, green — could it mean I have no bugs? impossible!)

--- solutions/SLE/Tests/UnitTests.fs
+++ solutions/SLE/Tests/UnitTests.fs
@@ -1,11 +1,21 @@
 ﻿namespace Tests
 
+open System.IO
 open SLE.Core
 open Microsoft.VisualStudio.TestTools.UnitTesting
 
+
 [<TestClass>]
 type UnitTests () =
-
+    member this.countFeatures (feature:Feature) : int =
+        1 +
+        Seq.sum [
+            feature.Mandatory |> Seq.sumBy this.countFeatures
+            feature.Optional |> Seq.sumBy this.countFeatures
+            feature.Alternative |> Seq.sumBy this.countFeatures
+            feature.Or |> Seq.sumBy this.countFeatures
+        ]
+    
     [<TestMethod>]
     member this.Trivial () =
         Assert.IsTrue(true)
@@ -22,3 +32,15 @@ type UnitTests () =
         Assert.AreEqual<int>(0, feature.Optional.Count)
         Assert.AreEqual<int>(0, feature.Alternative.Count)
         Assert.AreEqual<int>(0, feature.Or.Count)
+
+    [<TestMethod>]
+    member this.CountFeaturesInAutomotive01 () =
+        let grammar = parseGrammar metaModelPath
+        let features = Load(grammar, Path.Combine(Directory.GetCurrentDirectory().Split("solutions")[0], "models", "automotive01", "automotive01.uvl"))
+        Assert.AreEqual<int>(708+1805, this.countFeatures(features))
+
+    [<TestMethod>]
+    member this.CountFeaturesInAutomotive02 () =
+        let grammar = parseGrammar metaModelPath
+        let features = Load(grammar, Path.Combine(Directory.GetCurrentDirectory().Split("solutions")[0], "models", "automotive02", "automotive02_01.uvl"))
+        Assert.AreEqual<int>(1396+4071+8442+101, this.countFeatures(features))
